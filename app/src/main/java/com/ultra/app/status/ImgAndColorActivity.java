f5c2aed4efd8216d9d7e27176faeceef2ee988f7
qiaofangchun <qiaofc@uns1066.com>
2017-06-28 17:12:36 +0800

    添加状态栏适配

--- app/src/main/java/com/ultra/app/status/ImgAndColorActivity.java
+++ app/src/main/java/com/ultra/app/status/ImgAndColorActivity.java
@@ -3,12 +3,14 @@ package com.ultra.app.status;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.ultra.app.R;
 import com.ultra.statusbar.ImmersionBar;
 
-public class ImgAndColorActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+public class ImgAndColorActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     private Toolbar toolbar;
 
     @Override
@@ -16,10 +18,34 @@ public class ImgAndColorActivity extends AppCompatActivity implements SeekBar.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img_and_color);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        ImmersionBar.with(this).titleBar(toolbar).init();
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ((SeekBar)findViewById(R.id.seek_bar)).setOnSeekBarChangeListener(this);
+        ImmersionBar.with(this)
+                .titleBar(toolbar)
+                .navigationBarColor(R.color.colorPrimary)
+                .fullScreen(true)
+                .addTag("PicAndColor")  //给上面参数打标记，以后可以通过标记恢复
+                .init();
+        findViewById(R.id.btn_color).setOnClickListener(this);
+        findViewById(R.id.btn_status_color).setOnClickListener(this);
+        findViewById(R.id.btn_navigation_color).setOnClickListener(this);
+        ((SeekBar) findViewById(R.id.seek_bar)).setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_status_color:
+                ImmersionBar.with(this).statusBarColor(R.color.colorAccent).init();
+                break;
+            case R.id.btn_navigation_color:
+                if (ImmersionBar.hasNavigationBar(this))
+                    ImmersionBar.with(this).navigationBarColor(R.color.colorAccent).init();
+                else
+                    Toast.makeText(this, "当前设备没有导航栏", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_color:
+                ImmersionBar.with(this).getTag("PicAndColor").init(); //根据tag标记来恢复
+                break;
+        }
     }
 
     @Override
